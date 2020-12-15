f3557f46bc909181f4e517ab754d3991279a6da8
Ramiro Alegre <srrama11alegre@gmail.com>
2020-12-15 16:29:42 -0300

    formulario andando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RamiComponent } from './rami/rami.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { RamiComponent } from './rami/rami.component';
     RamiComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/rami/rami.component.ts
+++ src/app/rami/rami.component.ts
@@ -1,19 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { allowedNodeEnvironmentFlags } from 'process';
+import { __values } from 'tslib';
 
 @Component({
   selector: 'app-rami',
   templateUrl: './rami.component.html',
   styleUrls: ['./rami.component.css']
 })
-export class RamiComponent implements OnInit {
+export class RamiComponent  {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  FormularioFormGroup:FormGroup
 
-    console.log("ESTOY IMPRIMIENTO");
-    console.log("enviar fomulario");
 
-  }
+
+
+  constructor(
+
+    private _builder:FormBuilder
+
+  ) { this.FormularioFormGroup= this._builder.group ({
+    nombre: ["", ],
+    descripcion:["", ],
+    genero: ["",],
+
+    })}
+
+  //ngOnInit(): void {
+  mostrar (values){
+    console.log(values)};
+   
 
 }
+
+
